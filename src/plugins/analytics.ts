879484f332a82c57599e676f9cb0f10896a227e5
hg.park <hg.park@fromthered.com>
2020-12-18 15:21:06 +0900

    애널리틱스 수정

--- src/plugins/analytics.ts
+++ src/plugins/analytics.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue';
-import VueAnalytics from 'vue-analytics'
+import VueGtag from "vue-gtag";
 import router from './../router';
 
-Vue.use(VueAnalytics,{
-    id: 'G-9Y6WQ70RQ2', // 콘솔에서 본인의 키를 확인하세요!
-    router // 원활한 트래킹을 위해 router를 꼭 바인드 해 주어야 합니다!
-})
+Vue.use(VueGtag,{
+    config:{
+        id: 'G-HDLWZYZFBD'
+    },
+}, router);
