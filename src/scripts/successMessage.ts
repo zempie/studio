2b05c3e1b2a1c479259b7d7b872eec21ea84be54
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-03-05 15:44:07 +0900

    성공 메세지 정리

---
+++ src/scripts/successMessage.ts
@@ -0,0 +1,30 @@
+const SuccessMessage={
+
+    /* 로딩 관련 */
+    LOADING: '로딩중입니다...',
+    WAITING: '잠시만 기다려주세요',
+    CHECK_FILE: '파일을 확인하고 있습니다. 잠시만 기다려 주세요.',
+
+    /* 게임 버전 */
+    VERSION_FILE_OK:'파일이 정상적으로 업로드 되었습니다.',
+    VERSION_UPLOAD_OK:'새 버전이 등록되었습니다.',
+
+    /* 배포 */
+    DEPLOY_OK:'',
+    DEPLOY_VERSION_OK:'해당 버전이 정상적으로 배포되었습니다.',
+    UNDEPLOY_OK:'게임이 비공개 되었습니다.',
+    
+
+    /* 게임 등록 */
+    GAME_UPLOAD_OK:'게임이 정상적으로 등록되었습니다',
+    GAME_EDIT_OK:'수정 사항이 저장되었습니다',
+
+
+
+
+}
+
+
+export {
+    SuccessMessage,
+}
